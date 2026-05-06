2cd52f96a11d8b5853a86c2ac3a747f4115b0404
www1982 <victoryw159@gmail.com>
2026-05-06 21:10:39 +0800

    Add Patches/StateChangeEventsPatch.cs

---
+++ Patches/StateChangeEventsPatch.cs
@@ -0,0 +1,27 @@
+using HarmonyLib;
+using EZPlay.Core;
+using EZPlay.Core.Interfaces;
+using UnityEngine;
+
+namespace EZPlay.Patches
+{
+    // 捕获间歇泉状态变更事件
+    [HarmonyPatch(typeof(Geyser), "OnStateChanged")]
+    public class GeyserStateChangePatch
+    {
+        private static IEventBroadcaster _eventBroadcaster => ServiceContainer.Resolve<IEventBroadcaster>();
+
+        public static void Postfix(Geyser __instance, Geyser.States old_state, Geyser.States new_state)
+        {
+            var position = __instance.transform.position;
+            _eventBroadcaster?.BroadcastEvent("StateChange.GeyserStateChanged", new
+            {
+                worldId = __instance.GetMyWorldId(),
+                GeyserName = __instance.GetProperName(),
+                Position = new { X = position.x, Y = position.y },
+                OldState = old_state.ToString(),
+                NewState = new_state.ToString()
+            });
+        }
+    }
+}
